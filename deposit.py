b6b8045f88e1a043ab8d5482bd85c4ec5a92c392
Renato Souza <tttrenato@hotmail.com>
2024-08-31 17:44:20 -0300

    Fix: add: datetime to bank statement

--- deposit.py
+++ deposit.py
@@ -1,9 +1,11 @@
+from datetime import datetime
+
 def deposit(bank_balance, bank_statement):
     deposit_value = float(input('Digite o valor do depósito: '))
     if deposit_value <= 0:
         print('Valor inválido')
     else:
         bank_balance += deposit_value
-        bank_statement.append(f'Depósito: R$ {deposit_value}')
+        bank_statement.append(f'Depósito: R$ {deposit_value:.2f} data: {datetime.now().strftime("%H:%M:%S %d-%m-%Y")}')
         print(f'Depósito de R$ {deposit_value:.2f} efetuado com sucesso!')
     return bank_balance, bank_statement
