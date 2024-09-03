c0279569dc6f2324bfebf2b26053c6b2f43cc6c4
Renato Souza <tttrenato@hotmail.com>
2024-09-03 17:50:32 -0300

    feat: provisional logic for main

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from deposit import deposit
+
+
 menu = """
     [d] - Depositar
     [s] - Sacar
     [e] - Extrato
-    [q] - Sair
+    [q] - Sair\n
     """
     
 bank_balance = 0
@@ -16,18 +19,23 @@ while True:
     
     option = input(menu)
     option = option.lower()
-    print(option)
-    if option == 'd':
-        print('Deposito/n')
-        #logic to deposit
-    elif option == 's':
-        print('Saque/n')
-        #logic to withdraw
-    elif option == 'e':
-        print('Extrato/n')
-        #logic to show statement
-    elif option == 'q':
-        print('Sair/n')
-        break
-    else:
-        print('Opção inválida/n')
+    try:
+        if option == 'd':
+            print('Deposito\n')
+            bank_balance, bank_statement = deposit(bank_balance, bank_statement)
+        elif option == 's':
+            print('Saque\n')
+            #logic to withdraw
+        elif option == 'e':
+            print('Extrato\n')
+            #logic to show statement
+        elif option == 'q':
+            print('Sair\n')
+            break
+        else:
+                print('Opção inválida/n')
+    except Exception:
+        print('Valor inválido\n')
+    
+    print(f'Saldo: R$ {bank_balance:.2f}\n')
+    print(f'extrato: {bank_statement}\n')
