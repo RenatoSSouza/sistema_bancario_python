c000d7ebd62715e9a4de52d0bf54b4260a93664c
Renato Souza <tttrenato@hotmail.com>
2024-09-04 20:15:39 -0300

    fix: presentation of the current balance on the statement

--- bank_statement.py
+++ bank_statement.py
@@ -1,29 +1,27 @@
 from datetime import datetime
 
+from services.convert_input import locate_value_br
+
 def print_statement(bank_balance, bank_statement):
-    # Nome do banco e data do extrato
+
     bank_name = "DioBank"
     current_date = datetime.now().strftime("%d-%m-%Y")
     
-    # Cabeçalho do extrato
     print(f"\n{'='*40}")
     print(f"{bank_name:^40}")
     print(f"Data do Extrato: {current_date:^40}")
     print(f"{'='*40}")
     
-    # Verificar se há movimentações
     if not bank_statement:
         print("Não foram realizadas movimentações.".center(40))
     else:
-        # Cabeçalho da tabela
         print(f"{'Movimentação':<40}")
         print(f"{'-'*40}")
         
-        # Exibir movimentações
         for transaction in bank_statement:
             print(f"{transaction:<40}")
     
-    # Exibir saldo atual
+    formatted_value = str(locate_value_br(bank_balance))
     print(f"{'-'*40}")
-    print(f"{'Saldo Atual':<30} R$ {bank_balance:>8.2f}")
+    print(f"{'Saldo Atual':<30} {formatted_value}")
     print(f"{'='*40}\n")
